440afbd6f9909c5a6091b606990fd4e0d7884e10
Kesit Budi Kusumo <kesit@kernel.id>
2023-12-10 20:35:41 +0700

    initial commit

---
+++ src/main/java/id/kernel/bootcamp/springschedulingtasks/SpringSchedulingTasksApplication.java
@@ -0,0 +1,13 @@
+package id.kernel.bootcamp.springschedulingtasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSchedulingTasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSchedulingTasksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/kernel/bootcamp/springschedulingtasks/SpringSchedulingTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package id.kernel.bootcamp.springschedulingtasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSchedulingTasksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
